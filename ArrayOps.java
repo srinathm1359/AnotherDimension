0d5ba7a31155cc263fdeb5f28b641178c6b4c5a4
Srinath Mahankali <smahankali10@stuy.edu>
2020-10-22 14:22:12 -0400

    Added sum()

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,3 +1,9 @@
 public class ArrayOps {
-  
+  public static int sum(int[] arr) {
+    int totalSum = 0;
+    for (int i = 0; i < arr.length; i++) {
+      totalSum += arr[i];
+    }
+    return totalSum;
+  }
 }
