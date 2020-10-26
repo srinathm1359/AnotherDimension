d2022d829bcf584fdb0bdac985f641e2c337637b
Srinath Mahankali <smahankali10@stuy.edu>
2020-10-26 01:42:44 -0400

    Added test case for sumRows()

--- Tester.java
+++ Tester.java
@@ -2,10 +2,12 @@ import java.util.Arrays;
 public class Tester {
   public static void main(String[] args) {
     int[] arrOneDimension = {1,2,3};
-    System.out.println("Testing sum() on array {1,2,3}");
+    System.out.println("Testing sum() on array " + Arrays.toString(arrOneDimension));
     System.out.println(ArrayOps.sum(arrOneDimension));
-    System.out.println("Testing largest() on array {1,2,3}");
+    System.out.println("Testing largest() on array " + Arrays.toString(arrOneDimension));
     System.out.println(ArrayOps.largest(arrOneDimension));
+    int[][] arrTwoDimension = {{2,3,5},{7,11,13}};
+    System.out.println("Testing sumRows() on array " + Arrays.deepToString(arrTwoDimension));
+    System.out.println(Arrays.toString(ArrayOps.sumRows(arrTwoDimension)));
   }
-
 }
