6035b9adddec41a530bac7cde33c44890865ad93
Srinath Mahankali <smahankali10@stuy.edu>
2020-10-26 02:53:57 -0400

    Added newlines after print statements

--- Tester.java
+++ Tester.java
@@ -3,19 +3,19 @@ public class Tester {
   public static void main(String[] args) {
     int[] arrOneDimension = {1,2,3};
     System.out.println("Testing sum() on array " + Arrays.toString(arrOneDimension));
-    System.out.println(ArrayOps.sum(arrOneDimension));
+    System.out.println(ArrayOps.sum(arrOneDimension) + "\n");
     System.out.println("Testing largest() on array " + Arrays.toString(arrOneDimension));
-    System.out.println(ArrayOps.largest(arrOneDimension));
+    System.out.println(ArrayOps.largest(arrOneDimension) + "\n");
     int[][] arrTwoDimension = {{2,3,5},{7,11,13}};
     System.out.println("Testing sumRows() on array " + Arrays.deepToString(arrTwoDimension));
-    System.out.println(Arrays.toString(ArrayOps.sumRows(arrTwoDimension)));
+    System.out.println(Arrays.toString(ArrayOps.sumRows(arrTwoDimension)) + "\n");
     System.out.println("Testing largestInRows() on array " + Arrays.deepToString(arrTwoDimension));
-    System.out.println(Arrays.toString(ArrayOps.largestInRows(arrTwoDimension)));
+    System.out.println(Arrays.toString(ArrayOps.largestInRows(arrTwoDimension)) + "\n");
     System.out.println("Testing sum() on array " + Arrays.deepToString(arrTwoDimension));
-    System.out.println(ArrayOps.sum(arrTwoDimension));
+    System.out.println(ArrayOps.sum(arrTwoDimension) + "\n");
     System.out.println("Testing transpose() on array" + Arrays.deepToString(arrTwoDimension));
-    System.out.println(Arrays.deepToString(ArrayOps.transpose(arrTwoDimension)));
+    System.out.println(Arrays.deepToString(ArrayOps.transpose(arrTwoDimension)) + "\n");
     System.out.println("Testing sumCols() on array" + Arrays.deepToString(arrTwoDimension));
-    System.out.println(Arrays.toString(ArrayOps.sumCols(arrTwoDimension)));
+    System.out.println(Arrays.toString(ArrayOps.sumCols(arrTwoDimension)) + "\n");
   }
 }
