8c9a5211cc7f2c52a71f7044170c62b863697138
Srinath Mahankali <smahankali10@stuy.edu>
2020-10-26 01:34:34 -0400

    Added test case for sum() and largest()

--- Tester.java
+++ Tester.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
 public class Tester {
   public static void main(String[] args) {
-    
+    int[] arrOneDimension = {1,2,3};
+    System.out.println("Testing sum() on array {1,2,3}");
+    System.out.println(ArrayOps.sum(arrOneDimension));
+    System.out.println("Testing largest() on array {1,2,3}");
+    System.out.println(ArrayOps.largest(arrOneDimension));
   }
+
 }
